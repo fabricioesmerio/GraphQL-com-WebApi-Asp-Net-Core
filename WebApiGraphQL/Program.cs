eb746e203a6641f1e6041aa8d32822d4e85f5236
Fabricio Esmerio <fabricio1esmerio@gmail.com>
2024-01-18 00:36:51 -0300

    Ajustado Program.cs

--- WebApiGraphQL/Program.cs
+++ WebApiGraphQL/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using WebApiGraphQL.Application.Services;
+using WebApiGraphQL.GraphQL.Mutation;
+using WebApiGraphQL.GraphQL.Queries;
 using WebApiGraphQL.Infrastructure;
+using WebApiGraphQL.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +18,23 @@ builder.Services.AddDbContext<AppContextClass>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<IPostService, PostService>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<ICommentService, CommentService>();
+builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+builder.Services.AddScoped<IAuthorService, AuthorService>();
+
+builder.Services.AddGraphQLServer()
+    .AddQueryType(q => q.Name("Query"))
+    .AddType<PostQuery>()
+    .AddType<CommentQuery>()
+    .AddType<AuthorQuery>()
+    .AddMutationType(m => m.Name("Mutation"))
+    .AddType<AuthorMutation>()
+    .AddType<PostMutation>()
+    .AddType<CommentMutation>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +43,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGraphQL();
 
 app.UseHttpsRedirection();
 
