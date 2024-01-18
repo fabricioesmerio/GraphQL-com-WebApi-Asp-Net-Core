54983bbebb078339e304bc7456348145b0e3e149
Fabricio Esmerio <fabricio1esmerio@gmail.com>
2024-01-18 00:36:35 -0300

    Adicionado Query

---
+++ WebApiGraphQL/GraphQL/Query.cs
@@ -0,0 +1,13 @@
+﻿using WebApiGraphQL.Application.Services;
+using WebApiGraphQL.Domain.Entities;
+
+namespace WebApiGraphQL.GraphQL
+{
+    public class Query
+    {
+        public List<Author> GetAllAuthor([Service] IAuthorService _service)
+        {
+            return _service.GetAll();
+        }
+    }
+}
